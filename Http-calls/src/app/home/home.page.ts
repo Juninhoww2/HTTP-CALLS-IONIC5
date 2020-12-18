fce3202647f52f0f244f15392d31ce603d035fa0
juninhoww2 <alqadianokira@gmail.com>
2020-12-18 12:52:06 -0300

    failhttp

--- Http-calls/src/app/home/home.page.ts
+++ Http-calls/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  moedas: any;  
 
-  constructor() {}
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
-}
+  ngOnInit() {
+   const usd = this.activatedRoute.snapshot.paramMap.get('USD-BRL,CAD-BRL');
+   this.http.get(`https://economia.awesomeapi.com.br/all${usd}`).subscribe(res => {
+    this.moedas = res;
+   });
+  } 
+
+} 
